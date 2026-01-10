38e1233b06e7c9aa00589a9cfb75a7c7eca291ea
Naufal Bagaskara <naufal.bagaskara-2024@vokasi.unair.ac.id>
2026-01-10 23:16:47 +0700

    nunda lagi

--- day-01/Nopal_learn.java
+++ day-01/Nopal_learn.java
@@ -2,5 +2,6 @@ public class Nopal_learn {
     public static void main(String[] args) {
         System.out.println("I wish somehting big come to u - day 1");
         System.out.println("i wish something big come to u my friends - day 2");
+        System.out.println("i wish something big come to u my friends, thank u for helping me - day 3");
     }
 }
